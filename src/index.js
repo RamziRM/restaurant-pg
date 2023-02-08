a279ee292017c37be8d036d700b7dda920305b49
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-02-08 19:39:10 -0300

    intial module setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log('Hello world');
+// create page layout through js modules -- Reference accordingly in main.js
+
+// import all parts -- Setup Header - footer -- Navbar -EL's, link, etc.
+
