97f4cc2e72c659592a3d89339c9008421f0f8912
Ramzi <115042042+RamziRM@users.noreply.github.com>
2023-02-10 11:21:47 -0300

    contact module done

--- src/contact.js
+++ src/contact.js
@@ -1,1 +1,43 @@
 // Name - contact info
+
+createContact = () => {
+    const contact = document.createElement('div');
+    contact.classList.add('contact');
+
+    contact.appendChild(
+        contactForm(
+            "Don Mario",
+            "+5491231232",
+            'mario@luigipizza.com'
+        )
+    )
+
+    contact.appendChild(
+        contactForm(
+            "Don Luigi",
+            "+5412111223",
+            'luigi@luigipizza.com'
+        )
+    )
+};
+
+// contact creation f()-- Name - number - email //inputs
+function contactForm(fullName, number, email) {
+    const contactInput = document.createElement('div');
+    contactInput.classList.add('contact-input');
+
+    const fullName = document.createElement('h3');
+    fullName.textContent = fullName;
+
+    const number = document.createElement('p');
+    number.textContent = number;
+
+    const email = document.createElement('p');
+    email.textContent = email;
+
+    contactInput.appendChild(fullName);
+    contactInput.appendChild(Number);
+    contactInput.appendChild(email);
+
+    return contactInput;
+}
